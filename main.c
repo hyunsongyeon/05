c6b671721e312b03399ddfc8100dd3847e93a94c
hyunsongyeon <55427981+hyunsongyeon@users.noreply.github.com>
2019-10-01 15:41:14 +0900

    02

--- main.c
+++ main.c
@@ -4,15 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
 	int input;
 	
 	printf("input a number :");
 	scanf("%d", &input);
 	
 	if(input > 0)
-		printf("positive number \n");
+		printf("|x| = %d\n", input);
 	else
-		printf("negative number \n");
+		printf("|x| = %d\n", -input);
+	
 		
 	return 0;
 }
